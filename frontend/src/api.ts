c7ff861fd445983f32f015b8effa5187d007f1c9
rocky0shao <rockyshao22@gmail.com>
2026-01-07 18:56:18 -0500

    Fix hardcoded API URL

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 // Forcing a redeploy to pick up env varss
 // 1. Point to your Django Server
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
-
+// const API_BASE_URL = 'http://127.0.0.1:8000/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api';
+console.log("API URL being used:", API_BASE_URL);
 const getAuthHeaders = () => {
     const token = localStorage.getItem('token');
     return token ? { Authorization: `Token ${token}` } : {};
